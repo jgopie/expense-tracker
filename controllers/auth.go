bc6540ae61d3b83ea86a19bd6c032e2e50908a4a
jgopie <jordangopie@live.com>
2025-04-26 01:47:29 -0400

    Implement login functionality with JWT token generation and error handling

--- controllers/auth.go
+++ controllers/auth.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"expense-tracker/config"
 	"expense-tracker/models"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -26,4 +28,32 @@ func Register(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "error with user registration"})
 	}
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid request"})
+	}
+	var user models.User
+	config.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.ID == 0 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid username or password"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"])); err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid username or password"})
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenStr, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": tokenStr})
 }
