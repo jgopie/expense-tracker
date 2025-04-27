c7b1bb91809527168a8dcfd0c7c2d1cc1776773c
jgopie <jordangopie@live.com>
2025-04-27 00:28:40 -0400

    Remove frontend code and assets for expense tracker application
    - Deleted package.json, main.tsx, App.tsx, and other related files from the frontend directory.
    - Removed all CSS and SVG assets associated with the frontend.
    - Cleared out all TypeScript configuration files.
    - Updated server log to reflect recent API interactions.
    - Added CORS middleware to the Go server for handling requests from the frontend.

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	config.ConnectDB()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
 	file, err := os.OpenFile("/root/logs/server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatalf("failed to open log file %v", err)
@@ -22,5 +29,5 @@ func main() {
 	app.Use(middleware.Logger(logger))
 	routes.AuthRoutes(app)
 	routes.ExpenseTrackerRoutes(app)
-	app.Listen(":3000")
+	app.Listen("0.0.0.0:3000")
 }
